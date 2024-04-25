444919e7d321a1e8ee44fd0da3117a9b8d2e265f
xocass <joaquin.quintas@rai.usc.es>
2024-04-25 20:24:01 +0200

    jjuujj

--- src/main/java/baseDatos/daoPlaylist.java
+++ src/main/java/baseDatos/daoPlaylist.java
@@ -2,8 +2,12 @@ package baseDatos;
 
 import aplicacion.Contenido;
 import aplicacion.FachadaAplicacion;
+import aplicacion.Playlist;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class daoPlaylist extends AbstractDAO{
@@ -14,9 +18,31 @@ public class daoPlaylist extends AbstractDAO{
         super.setFachadaAplicacion(fa);
     }
 
-    public ArrayList<Contenido> playlistDefecto(){
-        ArrayList<Contenido> resultado=new ArrayList<>();
+    public ArrayList<Playlist> playlistDefecto(){
+        ArrayList<Playlist> resultado=new ArrayList<>();
+        Connection con;
+        String aux="spotify";
+        PreparedStatement stmPlaylist=null;
+        ResultSet rsPlaylist;
 
+        con=this.getConexion();
+
+        try {
+            stmPlaylist=con.prepareStatement("select * "+
+                    "from playlist "+
+                    "where idoyente=?");
+            stmPlaylist.setString(1,aux);
+            rsPlaylist=stmPlaylist.executeQuery();
+            while (rsPlaylist.next())
+            {
+                resultado.add(new Playlist(rsPlaylist.getString("nombre"),rsPlaylist.getString("idoyente")));
+            }
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+            this.getFachadaAplicacion().muestraExcepcion(e.getMessage());
+        }finally{
+            try {stmPlaylist.close();} catch (SQLException e){System.out.println("Imposible cerrar cursores");}
+        }
         return resultado;
     }
 
--- src/main/java/gui/cTemplateCancion.java
+++ src/main/java/gui/cTemplateCancion.java
@@ -1,6 +1,21 @@
 package gui;
 
-public class cTemplateCancion {
+import aplicacion.FachadaAplicacion;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
+public class cTemplateCancion {
+    @FXML
+    private Label labelUsuario;
+    private FachadaGui fgui;
+    private FachadaAplicacion fa;
+
+    public void setLabelUsuario(String text){
+        labelUsuario.setText(text);
+    }
+    public void setFachadas(FachadaGui fgui, FachadaAplicacion fa) {
+        this.fgui = fgui;
+        this.fa=fa;
+    }
 
 }
