849d4b0aebf58d77a8db7484fd1f44b94ef27a97
lupgz <lupgz59@gmail.com>
2024-04-25 19:03:54 +0200

    cAdmin

---
+++ src/main/java/gui/cOyenteMod.java
@@ -0,0 +1,14 @@
+package gui;
+
+import aplicacion.FachadaAplicacion;
+
+public class cOyenteMod {
+    private FachadaGui fgui;
+    private FachadaAplicacion fa;
+
+    public void setFachadas(FachadaGui fgui, FachadaAplicacion fa){
+        this.fgui=fgui;
+        this.fa=fa;
+    }
+
+}
